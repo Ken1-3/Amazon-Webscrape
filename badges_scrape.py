5f04be7defc8c980022df0ca60b1ec36aeed442b
Ken <89386946+Ken1-3@users.noreply.github.com>
2022-01-06 17:42:28 -0800

    Update badges_scrape.py

--- badges_scrape.py
+++ badges_scrape.py
@@ -1,88 +1,87 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 
-
-#add your asins in this list
-asins = ['']
-
-#create an empty list
-asin_link = []
-
-#create links with super link
-for i in asins:
-    link_start = 'https://www.amazon.com/dp/'
-    link = link_start + i
-    asin_link.append(link)
-
-#create dict from lists
-asin_list = dict(zip(asins, asin_link))
-
-#empty list for returned val
-value_list = []
-
-#path to webdriver
-driver = webdriver.Edge(r'path')
-
-#For loop to extract data
-for key in asin_list:
-   
-    #get link for asin
-    link = str(asin_list[key])
-
-    #try to find the best sellers badge
-    try:
-        #execute link
-        driver.get(link)
-        #find element
-        badge=driver.find_element(By.CLASS_NAME,"ac-badge-wrapper").text
+def badge_scrape(asins: list):
+    #add your asins in this list
     
-        #format element
-        x = int(badge.find('by'))
-        badge.strip('  ')
-        badge = (badge.replace("\n", " "))
-        badge = badge[0:x]
-        badge = badge.replace('for','in')
-        badge = badge.split('in',1)
-        
-        #more filtering of returned data
-        try:
-            badge = badge.split('for')
-        except:
-            pass        
-        badge_list = []
-        for i in badge:
-            badge = i.strip()            
-            badge_list.append(badge)
-        value_list.append(badge_list)
+    #create an empty list
+    asin_link = []
     
-    except:
-        
-        #try to find the #1 badge
+    #create links with super link
+    for i in asins:
+        link_start = 'https://www.amazon.com/dp/'
+        link = link_start + i
+        asin_link.append(link)
+    
+    #create dict from lists
+    asin_list = dict(zip(asins, asin_link))
+    
+    #empty list for returned val
+    value_list = []
+    
+    #path to webdriver
+    driver = webdriver.Edge(r'path')
+    
+    #For loop to extract data
+    for key in asin_list:
+       
+        #get link for asin
+        link = str(asin_list[key])
+    
+        #try to find the best sellers badge
         try:
             #execute link
             driver.get(link)
-            
             #find element
-            badge=driver.find_element(By.CLASS_NAME,"badge-link").text
-           
-            #formatting
+            badge=driver.find_element(By.CLASS_NAME,"ac-badge-wrapper").text
+        
+            #format element
+            x = int(badge.find('by'))
+            badge.strip('  ')
+            badge = (badge.replace("\n", " "))
+            badge = badge[0:x]
+            badge = badge.replace('for','in')
             badge = badge.split('in',1)
+            
+            #more filtering of returned data
+            try:
+                badge = badge.split('for')
+            except:
+                pass        
             badge_list = []
             for i in badge:
-                badge = i.strip()
+                badge = i.strip()            
                 badge_list.append(badge)
             value_list.append(badge_list)
-
-        #define if no badges
+        
         except:
-            na_val = 'N/A N/A'
-            na_val = na_val.split(' ')
-            value_list.append(na_val)
+            
+            #try to find the #1 badge
+            try:
+                #execute link
+                driver.get(link)
+                
+                #find element
+                badge=driver.find_element(By.CLASS_NAME,"badge-link").text
+               
+                #formatting
+                badge = badge.split('in',1)
+                badge_list = []
+                for i in badge:
+                    badge = i.strip()
+                    badge_list.append(badge)
+                value_list.append(badge_list)
+    
+            #define if no badges
+            except:
+                na_val = 'N/A N/A'
+                na_val = na_val.split(' ')
+                value_list.append(na_val)
+    
+    #put results in dict
+    return_dict = dict(zip(asin_list,value_list))
+    return_string = json.dumps(return_dict)
+    
+    return return_string
 
-#put results in dict
-return_dict = dict(zip(asin_list,value_list))
-return_string = json.dumps(return_dict)
-
-print(return_string)
